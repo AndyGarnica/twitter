0c19b8b6b8821d5add91ed85f5dae43f8d815fd4
Elsa Andrea Garnica Reyes <elsaandreagarnicareyes@MacBook-Air-de-Elsa.local>
2017-06-01 16:23:22 -0500

    estilo

--- js/app.js
+++ js/app.js
@@ -1,21 +1,70 @@
-window.addEventListener("load", function() {
-  var toDoForm = document.getElementById('to-do-form');
-  toDoForm.addEventListener("submit", function (e){
-    e.preventDefault();
-    var contenedor = document.getElementById("posts");
-    var mensajeContenedor = document.getElementById('message');
-    var mensaje = mensajeContenedor.value;
 
-    var postContenedor = document.createElement("article");
-    var post = document.createElement("p");
+// IIFE - Immediately Invoked Function Expression
+(function () {
+	var contador = 0;
 
-    postContenedor.className = "jumbotron";
-    post.textContent = mensaje;
+	var cargarPagina = function () {
+		// Envío de Publicación
+		$("#to-do-form").submit(agregarToDo);
+		$("#message").keyup(validarContenido);
+	};
 
-		postContenedor.appendChild(post);
+	var agregarToDo = function (e) {
+		e.preventDefault();
+		// Obtenemos datos
+		var $contenedor = $("#posts");
+		var $mensajeContenedor = $("#message");
+		var $botonAgregar = $("#add-button");
+		var mensaje = $mensajeContenedor.val();
 
-		contenedor.appendChild(postContenedor);
 
-		mensajeContenedor.value = "";
-  });
-});
+		// Creamos elementos
+		var $postContenedor = $("<article />", { "class": "jumbotron" });
+		var $postCheck = $("<input type='checkbox' />");
+		var $postTexto = $("<label />");
+
+		var identificador = "marcador-" + contador;
+
+		// Personalizamos elementos
+		// $postContenedor.addClass("jumbotron");
+		$postCheck.id = identificador;
+		// $postCheck.type = "checkbox";
+		$postTexto.attr("for", identificador);
+		$postTexto.text(mensaje);
+
+		$postCheck.click(eliminarToDo);
+
+		// Agregarlos al DOM
+		$postContenedor.append($postCheck);
+		$postContenedor.append($postTexto);
+
+		// Agregarlo a un elemento existente para visualizarlo
+		// contenedor.appendChild(postContenedor);
+		$contenedor.prepend($postContenedor);
+
+		// Borrar contenido de textarea
+		$mensajeContenedor.val("");
+		$botonAgregar.attr("disabled", true);
+
+		// bind, apply, call
+
+		contador++;
+	};
+
+	var eliminarToDo = function () {
+		$(this).parent().remove();
+	};
+
+	var validarContenido = function () {
+		var $addButton = $("#add-button");
+		// .trim() solo borra los espacios de sobra a los costados (izquierda y derecha)
+		if($(this).val().trim().length > 0) {
+			$addButton.removeAttr("disabled");
+		} else {
+			$addButton.attr("disabled", true);
+		}
+	};
+
+	// Cuando carga la página
+	$(document).ready(cargarPagina);
+})();
